da7059d5429f2151774657e1fcdc71ed7e75a87b
MyTran2508 <mytranchi2508@mail.com>
2023-08-02 16:05:05 +0700

    update

--- BackEnd/src/main/java/com/ocena/qlsc/podetail/utils/FileExcelUtil.java
+++ BackEnd/src/main/java/com/ocena/qlsc/podetail/utils/FileExcelUtil.java
@@ -5,15 +5,10 @@ import com.ocena.qlsc.common.message.StatusMessage;
 import com.ocena.qlsc.common.response.ErrorResponseImport;
 import com.ocena.qlsc.common.response.ResponseMapper;
 import com.ocena.qlsc.common.util.DateUtil;
-import com.ocena.qlsc.common.util.SystemUtil;
 import com.ocena.qlsc.podetail.constants.ImportErrorType;
 import com.ocena.qlsc.podetail.constants.RegexConstants;
-import com.ocena.qlsc.user.model.Role;
-import com.ocena.qlsc.user.model.RoleUser;
-import com.ocena.qlsc.user.repository.RoleRepository;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,8 +17,6 @@ import java.util.*;
 
 @Service
 public class FileExcelUtil {
-    @Autowired
-    RoleRepository roleRepository;
     /**
      * Checks whether a given cell value matches a specified regular expression.
      *
@@ -42,7 +35,7 @@ public class FileExcelUtil {
      * @return an ErrorResponseImport object with an error message if the header values are invalid,
      * or null if the header values are valid
      */
-    public ErrorResponseImport validateHeaderValue(Iterator<Row> rowIterator, HashMap<Integer, String> map) {
+    public static ErrorResponseImport validateHeaderValue(Iterator<Row> rowIterator, HashMap<Integer, String> map) {
         try {
             if (rowIterator.hasNext()) {
                 Row row = rowIterator.next();
@@ -63,11 +56,11 @@ public class FileExcelUtil {
         return null;
     }
 
-    public Object getFieldsNameInHeader(Iterator<Row> rowIterator) {
+    public static Object getFieldsNameFromHeader(Iterator<Row> rowIterator) {
         List<String> fieldList = new ArrayList<>();
         if (rowIterator.hasNext()) {
             Row row = rowIterator.next();
-            List<String> validFields = new ArrayList<>(RegexConstants.fieldsRegexMap.keySet());
+            List<String> validFields = new ArrayList<>(RegexConstants.FIELDS_REGEX_MAP.keySet());
 
 
             for (int i = 0; i < row.getLastCellNum(); i++) {
@@ -76,14 +69,12 @@ public class FileExcelUtil {
                     return new ErrorResponseImport(ImportErrorType.HEADER_DATA_WRONG, "Cột Header thứ " + (i + 1) + " bị trùng với cột khác");
                 }
 
-                if (fieldsName.toLowerCase().matches(RegexConstants.regexProductName))
-                    continue;
 
                 boolean isHeaderValid = false;
                 for (String fieldRegex : validFields) {
                     if (isHeaderValid(fieldsName, fieldRegex)) {
                         isHeaderValid = true;
-                        fieldList.add(RegexConstants.fieldsRegexMap.get(fieldRegex));
+                        fieldList.add(RegexConstants.FIELDS_REGEX_MAP.get(fieldRegex));
                         break;
                     }
                 }
@@ -96,21 +87,8 @@ public class FileExcelUtil {
                 return new ErrorResponseImport(ImportErrorType.HEADER_DATA_WRONG, "Header bắt buộc phải có Mã HH - Số PO - Số Serial");
             }
         }
-
-        if (!validateRoleUpdatePO(fieldList)) {
-            return new ErrorResponseImport(ImportErrorType.NOT_PERMISSION, "Cập nhật SC hoặc KSC tương ứng với quyền");
-        }
-
-        fieldList.forEach(System.out::println);
         return fieldList;
     }
-
-    /**
-     * Checks whether a given cell contains a numeric value.
-     *
-     * @param cell the cell to be checked
-     * @return true if the cell contains a numeric value, false otherwise
-     */
     private boolean isNumericCell(Cell cell) {
         return cell != null && cell.getCellType() == CellType.NUMERIC;
     }
@@ -120,45 +98,13 @@ public class FileExcelUtil {
     }
 
     /**
-     * Validates whether the cells in a given row and specified columns contain numeric values.
-     *
-     * @param row           the row containing the cells to be validated
-     * @param rowIndex      the index of the row in the import file
-     * @param columnIndexes an array of column indices to be validated
-     * @return an ErrorResponseImport object with an error message if any of the cells do not contain numeric values,
-     * or null if all the cells contain numeric values
-     */
-    public Object validateNumbericColumns(Row row, int rowIndex, int... columnIndexes) {
-        for (int columnIndex : columnIndexes) {
-            Cell cell = row.getCell(columnIndex);
-            if (!isNumericCell(cell)) {
-                return new ErrorResponseImport(ImportErrorType.DATA_NOT_MAP, rowIndex,
-                        "Hàng " + rowIndex + " Cột " + (columnIndex + 1) + " không phải dạng Numberic");
-            }
-        }
-        return null;
-    }
-
-
-    public Object validateTextColumns(Row row, int rowIndex, int... columnIndexes) {
-        for (int columnIndex : columnIndexes) {
-            Cell cell = row.getCell(columnIndex);
-            if (!isTextCell(cell)) {
-                return new ErrorResponseImport(ImportErrorType.DATA_NOT_MAP, rowIndex,
-                        "Hàng " + rowIndex + " Cột " + (columnIndex + 1) + " không phải dạng Text");
-            }
-        }
-        return null;
-    }
-
-    /**
      * Processes an Excel file uploaded by the user.
      *
      * @param file the Excel file to be processed
      * @return an iterator over the rows in the Excel file if the file is valid,
      * or an ErrorResponseImport object with an error message if the file is invalid
      */
-    public Object getSheetIteratorFromExcelFile(MultipartFile file) {
+    public static Object getSheetIteratorFromExcelFile(MultipartFile file) {
         List<ErrorResponseImport> listError = new ArrayList<>();
 
         // Check whether the file has the correct format
@@ -182,7 +128,7 @@ public class FileExcelUtil {
         }
     }
 
-    public boolean isLastedRow(Row row) {
+    public static boolean isLastedRow(Row row) {
         if ((row.getCell(0) == null || row.getCell(0).getCellType() == CellType.BLANK) &&
                 (row.getCell(1) == null || row.getCell(1).getCellType() == CellType.BLANK) &&
                 (row.getCell(2) == null || row.getCell(2).getCellType() == CellType.BLANK)) {
@@ -195,7 +141,6 @@ public class FileExcelUtil {
         Row row = (Row) rowCell;
         int col = (int) colIndex;
         if(row.getCell(col) == null || row.getCell(col).getCellType() == CellType.BLANK) {
-            System.out.println("Vao day a");
             return "";
         }
         CellType cellType = row.getCell(col).getCellType();
@@ -235,22 +180,6 @@ public class FileExcelUtil {
                 return -1;
             }
         }
-    }
-
-    public Boolean validateRoleUpdatePO(List<String> attribute) {
-        String email = SystemUtil.getCurrentEmail();
-        List<Role> allRoles = roleRepository.getRoleByEmail(email);
-        List<String> fieldsKSCUpdate = new ArrayList<>(Arrays.asList("productId", "serialNumber", "poNumber", "kcsVT"));
-        List<String> fieldsRepairStatusUpdate = new ArrayList<>(Arrays.asList("productId", "serialNumber", "poNumber", "repairStatus"));
-
-        for (Role role : allRoles) {
-            if ((role.getRoleName().equals(RoleUser.ROLE_ADMIN.name()) || role.getRoleName().equals(RoleUser.ROLE_MANAGER.name()))
-                    || (role.getRoleName().equals(RoleUser.ROLE_KCSANALYST.name()) && attribute.equals(fieldsKSCUpdate)
-                    || (role.getRoleName().equals(RoleUser.ROLE_REPAIRMAN.name()) && attribute.equals(fieldsRepairStatusUpdate)))) {
-                return true;
-            }
-        }
-        return false;
     }
 }
 
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,20 +3,15 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "react-datepicker/dist/react-datepicker.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { UserProvider } from "./Context/UseContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes.jsx";
 import store from "./redux/store.jsx";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <React.StrictMode>
-      {/* <UserProvider> */}
         <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
-      {/* </UserProvider> */}
-    </React.StrictMode>
   </Provider>
 );
